0af40ba52a307816e941d729ac7560aaf5790c83
Thrilok <thrilokreddy.dev@gmail.com>
2022-11-19 21:09:24 -0600

    claim process app

---
+++ src/main/java/com/claims/controller/ClaimsController.java
@@ -0,0 +1,32 @@
+package com.claims.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.claims.dao.ClaimsDAO;
+import com.claims.model.Claims;
+
+@RestController
+public class ClaimsController {
+	
+	@Autowired
+	private ClaimsDAO claimsDAO;
+	
+	@GetMapping("/getClaimDetails/{claimId}")
+	public Claims getClaimStatus(@PathVariable int claimId){ 
+		Claims data = claimsDAO.findByClaimId(claimId);
+		return data;
+	}
+	
+	@GetMapping("/getAllClaims")
+	public List<Claims> getAllClaims(){ 
+		List<Claims> data = claimsDAO.findAll();
+		return data;
+	}
+	 
+
+}
